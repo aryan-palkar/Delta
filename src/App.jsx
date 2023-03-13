b29a12d2eb301a5183138249896f727a01600f42
Niyati <blu545815@gmail.com>
2023-03-13 16:37:52 +0530

    signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
 import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import { BrowserRouter, Route, Routes,Link } from 'react-router-dom';
+
 function App() {
 
   return (
-    <Home/>
+    <>
+    
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+          <Link to="/signup">Sign Up</Link>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    </nav>
+  
+    <Routes>
+      <Route path="/" element={<Home/> }></Route>
+      <Route path="/signup" element={<Signup/>} ></Route>
+      <Route path="/login" element={<Login/>} ></Route>
+    </Routes>
+    </>
   );
 }
 
---
+++ src/pages/Login.jsx
@@ -0,0 +1,8 @@
+
+export default function Login(){
+    return(
+        <div>
+            Ulu ulu
+        </div>
+    )
+} 
