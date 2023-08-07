bc7b7ea717648ed1986a1d721a3d437fc73b83b2
aryan-palkar <aryan.palkar02@gmail.com>
2023-08-08 01:16:10 +0530

    bleh

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,34 @@
-import Home from './pages/Home';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import NewBlogPage from './pages/NewBlogPage';
-import { Route, Routes,Link } from 'react-router-dom';
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import NewBlogPage from "./pages/NewBlogPage";
+import { Route, Routes, Link } from "react-router-dom";
+import navStyle from "./styles/nav.module.css";
 
 function App() {
   return (
-    <>  
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-          <Link to="/signup">Sign Up</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/newblog">New Blog</Link>
-        </li>
-      </ul>
-    </nav>
-  
-    <Routes>
-      <Route path="/" element={<Home/> }/>
-      <Route path="/signup" element={<Signup/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/newblog" element={<NewBlogPage/>}/>
-    </Routes>
+    <>
+      <nav class={navStyle.nav}>
+        <Link class={navStyle.logo} to="/">Home</Link>
+        <ul>
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/newblog">New Blog</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/newblog" element={<NewBlogPage />} />
+      </Routes>
     </>
   );
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -35,10 +35,18 @@ export default function Signup(){
     return(
         <>
             <form>
+                <label>Email :</label>
                 <input type="email" name="email" value={info.email} onChange={changeHandler} />
+                <br/>
+                <label>Display name :</label>
                 <input type="text" name="displayName" value={info.displayName} onChange={changeHandler} />
+                <br/>
+                <label>Username :</label>
                 <input type="text" name="username" value={info.username} onChange={changeHandler} />
+                <br/>               
+                <label>Password :</label>
                 <input type="password" name="password" value={info.password} onChange={changeHandler} />
+                <br/>
                 <input type="submit" name="submit" onClick={submit}/>
             </form>
             <div>
